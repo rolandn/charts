1250fa287c394ee89b74f160fe58360914dbc2b0
Roland NEYRINCK <info@appalach.be>
2019-06-17 12:14:36 +0200

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "charts",
+
+    'summary': """
+        Module test pour graphique Heightcharts""",
+
+    'description': """
+        Le module pour tester la visualisation de graphique heightchats.
+    """,
+
+    'author': "Roland Neyrinck",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['sale'],
+
+    'data': ['report/report_sale_order_inherit.xml'],
+
+}
+
