ba39aa7a4e09e7c029997014b88b8544fd30c0e4
Roland NEYRINCK <info@appalach.be>
2019-06-17 12:31:09 +0200

    ajout du bdc

--- bdc_inherit.py
+++ bdc_inherit.py
@@ -14,3 +14,20 @@ class Sale(models.Model):
 
 class SaleOrder(models.Model):
     _inherit = ['sale.order.line']
+
+
+
+class ParticularReport(models.AbstractModel):
+    _name = 'report.charts.report_sale_order_inherit'
+    @api.model
+    def render_html(self, docids, data=None):
+        report_obj = self.env['report']
+        report = report_obj._get_report_from_name('charts.report_sale_order_inherit')
+        docargs = {
+            'doc_ids': docids,
+            'doc_model': report.model,
+            'docs': self,
+        }
+        return report_obj.render('charts.report_sale_order_inherit', docargs)
+
+
